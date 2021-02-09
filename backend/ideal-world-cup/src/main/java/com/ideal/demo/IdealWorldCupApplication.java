851ae53ad1591227a471e4b206d9a1b6d3252ff5
akak4456 <akak4456@naver.com>
2021-02-09 16:27:44 +0900

    commit

---
+++ backend/ideal-world-cup/src/main/java/com/ideal/demo/IdealWorldCupApplication.java
@@ -0,0 +1,13 @@
+package com.ideal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealWorldCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealWorldCupApplication.class, args);
+	}
+
+}
---
+++ backend/ideal-world-cup/src/test/java/com/ideal/demo/IdealWorldCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealWorldCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
