96cb0a10ff3cdfb31d9b106f914ae200669e0b4d
akak4456 <akak4456@naver.com>
2021-02-09 16:15:45 +0900

    commit

--- frontend/ideal-world-cup/src/components/ideal_world_cup/IdealWorldCupStartCard.jsx
+++ frontend/ideal-world-cup/src/components/ideal_world_cup/IdealWorldCupStartCard.jsx
@@ -27,7 +27,7 @@ class IdealWorldCupStartCard extends PureComponent {
               </Col>
             </Row>
           </Container>
-          <ButtonGroup style={{ padding: 0 }}>
+          <ButtonGroup size="lg" vertical style={{ display: 'flex', justifyContent: 'center' }}>
             <Button variant="outline-danger">시작하기</Button>
             <Button variant="outline-warning">랭킹보기</Button>
             <Button variant="outline-primary">공유</Button>
