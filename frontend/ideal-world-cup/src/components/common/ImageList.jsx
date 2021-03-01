3c211dee82ab7cf4ef2ad59f250addaec38a1239
akak4456 <akak4456@naver.com>
2021-03-01 16:43:46 +0900

    no message

--- frontend/ideal-world-cup/src/components/common/ImageList.jsx
+++ frontend/ideal-world-cup/src/components/common/ImageList.jsx
@@ -3,42 +3,15 @@ import { Card, CardColumns, Form, Button, ButtonGroup } from 'react-bootstrap';
 import DragAndDrop from './DragAndDrop';
 import { withTranslation } from 'react-i18next';
 class ImageList extends Component {
-  state = {
-    files: [],
-  };
-  handleDrop = (files) => {
-    for (var i = 0; i < files.length; i++) {
-      if (!files[i].name) return;
-      let reader = new FileReader();
-      let file = files[i];
-      reader.onloadend = () => {
-        const newFileItem = {
-          ...file,
-          previewURL: reader.result,
-        };
-        this.setState({
-          files: this.state.files.concat(newFileItem),
-        });
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-  removeFile = (id) => {
-    console.log(id);
-    const { files } = this.state;
-    this.setState({
-      files: files.filter((file, idx) => idx != id),
-    });
-  };
   render() {
-    const { t } = this.props;
+    const { t, handleDrop, handleRemove, files } = this.props;
     return (
-      <DragAndDrop handleDrop={this.handleDrop}>
-        {this.state.files.length == 0 ? (
+      <DragAndDrop handleDrop={handleDrop}>
+        {files.length == 0 ? (
           <p>{t('drag_and_drop_here')}</p>
         ) : (
           <CardColumns>
-            {this.state.files.map((file, idx) => (
+            {files.map((file, idx) => (
               <Card key={idx}>
                 <Card.Img variant="top" src={file.previewURL} />
                 <Card.Body>
@@ -53,7 +26,7 @@ class ImageList extends Component {
                     vertical
                     style={{ display: 'flex', justifyContent: 'center' }}
                   >
-                    <Button variant="danger" onClick={() => this.removeFile(idx)}>
+                    <Button variant="danger" onClick={() => handleRemove(idx)}>
                       {t('delete')}
                     </Button>
                   </ButtonGroup>
--- frontend/ideal-world-cup/src/containers/ideal_world_cup/IdealWorldCupMaker.jsx
+++ frontend/ideal-world-cup/src/containers/ideal_world_cup/IdealWorldCupMaker.jsx
@@ -4,6 +4,38 @@ import { Form, Button } from 'react-bootstrap';
 import ImageList from '../../components/common/ImageList';
 import { withTranslation } from 'react-i18next';
 class IdealWorldCupMaker extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleDrop.bind(this);
+    this.handleRemove.bind(this);
+    this.state = {
+      files: [],
+    };
+  }
+  handleDrop = (files) => {
+    for (var i = 0; i < files.length; i++) {
+      if (!files[i].name) return;
+      let reader = new FileReader();
+      let file = files[i];
+      reader.onloadend = () => {
+        const newFileItem = {
+          ...file,
+          previewURL: reader.result,
+        };
+        this.setState({
+          files: this.state.files.concat(newFileItem),
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  handleRemove = (id) => {
+    console.log(id);
+    const { files } = this.state;
+    this.setState({
+      files: files.filter((file, idx) => idx != id),
+    });
+  };
   render() {
     const { t } = this.props;
     return (
@@ -20,8 +52,14 @@ class IdealWorldCupMaker extends PureComponent {
           {t('save')}
         </Button>
         <Form.Group controlId="formImageList">
-          <Form.Label>{t('image_list')}</Form.Label>
-          <ImageList />
+          <Form.Label>
+            {t('image_list')}({t('count')}:{this.state.files.length})
+          </Form.Label>
+          <ImageList
+            handleDrop={this.handleDrop}
+            handleRemove={this.handleRemove}
+            files={this.state.files}
+          />
         </Form.Group>
       </Form>
     );
