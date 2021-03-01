0bfdd057f212b9c6d4296fc893ea73a77bd5119e
akak4456 <akak4456@naver.com>
2021-03-01 16:27:45 +0900

    commit

--- frontend/ideal-world-cup/src/containers/ideal_world_cup/IdealWorldCupMaker.jsx
+++ frontend/ideal-world-cup/src/containers/ideal_world_cup/IdealWorldCupMaker.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import ImageList from '../../components/common/ImageList';
 import { withTranslation } from 'react-i18next';
 class IdealWorldCupMaker extends PureComponent {
@@ -16,6 +16,9 @@ class IdealWorldCupMaker extends PureComponent {
           <Form.Label>{t('detail')}</Form.Label>
           <Form.Control type="text" placeholder={t('input_detail')} />
         </Form.Group>
+        <Button variant="primary" type="submit">
+          {t('save')}
+        </Button>
         <Form.Group controlId="formImageList">
           <Form.Label>{t('image_list')}</Form.Label>
           <ImageList />
